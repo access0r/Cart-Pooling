1225e58f54d4bb733cfd95718d9609b9d58ccc6b
accessor.eth <38918838+clxtho@users.noreply.github.com>
2023-05-11 09:54:30 -0700

    Update cartPool.sol

--- cartPool.sol
+++ cartPool.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract FundPool {
@@ -134,3 +135,4 @@ contract FundPool {
         delete pools[id];
         emit PoolDeleted(id);
     }
+}
